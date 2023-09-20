423ca743605ad368236c136d3556ab5f39113699
Victor Martins <VictorMLMartins@icloud.com>
2023-09-20 20:51:30 -0300

    feat: :sparkles: Adicionado features em Usuário Controllers
    Colocado os status HTTP e também as validações no usuário controller

--- src/controller/UsuarioController.js
+++ src/controller/UsuarioController.js
@@ -1,4 +1,6 @@
+import UsuarioModel from "../model/UsuarioModel.js"
 import UsuarioRepository from "../repository/UsuarioRepository.js"
+import ValidacaoUsuarioServices from "../services/ValidacaoUsuarioServices.js"
 
 class UsuarioController {
   /**
@@ -6,47 +8,80 @@ class UsuarioController {
    * @param { Express } app 
    */
   static rotas(app) {
-    //Inserir novo usuário
+    /**
+     * Método para criar novo usuário
+     */
     app.post('/usuarios', async (req, res) => {
-      const data = req.body
-      
-      res.status(200).json(
-        UsuarioRepository.inserirUsuario(data)
-      )
+      const data = Object.values(req.body)
+      const usuario = new UsuarioModel(...data)
+
+     if(ValidacaoUsuarioServices.ValidaCamposUsuario(...data)) {
+       res.status(201).json(
+         UsuarioRepository.inserirUsuario(usuario)
+       )
+     } else {
+        res.status(406).json({
+          error: true,
+          message: `Os campos inseridos são inválidos`
+        })
+     }
     })
 
-    //Buscar Todos os Usuários
+    /**
+     * Método para buscar todos os usuários
+     */
     app.get('/usuarios', (req, res) => {
       const buscaUsuarios = UsuarioRepository.buscarUsuario()
 
-      buscaUsuarios.then((result) => {
-        res.send(result)
-      })
-
+      if(buscaUsuarios) {
+        buscaUsuarios.then((result) => {
+          res.status(200).json(result)
+        })
+      } else {
+        res.status(404).json({
+          error: true,
+          message: `Não há usuários para mostrar`
+        })
+      }
     })
 
-    // Buscar usuário por id
+    /**
+     * Método para buscar um único usuário
+     */
     app.get('/usuarios/:id', (req, res) => {
       const id = req.params.id
       const buscaUsuarios = UsuarioRepository.buscarUsuario()
 
       buscaUsuarios.then((result) => {
-        res.send(result[id - 1])
+        res.status(200).json(result[id - 1])
       })
     })
 
-    //Fazer update de usuário
+    /**
+     * Método para fazer update de usuário por id
+     */
     app.put('/usuarios/:id', (req, res) => {
-      const data = req.body
+      const data = Object.values(req.body)
       const id = req.params.id
+      const usuario = new UsuarioModel(...data)
+      console.log(usuario)
       const buscaUsuarios = UsuarioRepository.buscarUsuario()
 
-      buscaUsuarios.then((result) => {
-        res.status(204).json(UsuarioRepository.updateUsuario(data, result[id - 1].id))
-      })
+      if(ValidacaoUsuarioServices.ValidaCamposUsuario(...data)) {
+        buscaUsuarios.then((result) => {
+          res.status(204).json(UsuarioRepository.updateUsuario(usuario, result[id - 1].id))
+        })
+      } else {
+      res.status(406).json({
+                error: true,
+                message: `Os campos inseridos são inválidos`
+              })
+           }
     })
 
-    //Fazer a deleção do usuário
+    /**
+     * Método para deletar usuário
+     */
     app.delete('/usuarios/:id', (req, res) => {
       const id = req.params.id
       const buscaUsuarios = UsuarioRepository.buscarUsuario()
