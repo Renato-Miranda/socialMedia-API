4533acc52c2c5bc6b9d1adccc791a3b55134782c
Victor Martins <VictorMLMartins@icloud.com>
2023-09-22 16:30:09 -0300

    feat: :sparkles: Atualização de Feed e Post Controller
    Atualizado as funcionalidades CRUD do feed e post controller, colocando as rotas corretas e as validações

--- src/controller/FeedController.js
+++ src/controller/FeedController.js
@@ -1,61 +1,119 @@
-import FeedRepository from "../repository/FeedRepository.js"
+import FeedModel from '../model/FeedModel.js';
+import FeedRepository from '../repository/FeedRepository.js';
+import UsuarioRepository from '../repository/UsuarioRepository.js';
 
 class FeedController {
   /**
-   * 
-   * @param { Express } app 
+   *
+   * @param { Express } app
    */
   static rotas(app) {
-    //Inserir novo feed
+    /**
+     * Método para criar feed
+     */
     app.post('/feed', async (req, res) => {
-      const data = req.body
-      
-      res.status(200).json(
-        FeedRepository.inserirFeed(data)
-      )
-    })
+      const data = Object.values(req.body);
+      const feed = new FeedModel(...data);
 
-    //Buscar Todos os feeds
+      if (feed) {
+        FeedRepository.inserirFeed(feed);
+        res.status(201).json({
+          success: true,
+          message: `Feed inserido com sucesso`,
+        });
+      } else {
+        res.status(406).json({
+          error: true,
+          message: 'Não foi possível inserir feed',
+        });
+      }
+    });
+
+    /**
+     * Método para buscar todos os feed
+     */
     app.get('/feed', (req, res) => {
-      const buscaFeed = FeedRepository.buscarFeed()
+      const todosFeed = FeedRepository.buscarFeed();
 
-      buscaFeed.then((result) => {
-        res.send(result)
-      })
+      if (todosFeed) {
+        todosFeed.then((feeds) => {
+          res.status(200).json(feeds);
+        });
+      } else {
+        res.status(404).json({
+          error: true,
+          message: `Não há feed para mostrar`,
+        });
+      }
+    });
 
-    })
-
-    // Buscar feed por id do usuário
+    /**
+     * Método para buscar um único feed por usuário
+     */
     app.get('/feed/:id', (req, res) => {
-      const id = req.params.id
-      const buscaFeed = FeedRepository.buscarFeed()
+      const id = req.params.id;
 
-      buscaFeed.then((result) => {
-        res.send(result[id - 1])
-      })
-    })
+      const usuarioBuscado = null;
+      const buscarTodosUsuarios = UsuarioRepository.buscarUsuario();
 
-    //Fazer update do feed
-    app.put('/feed/:id', (req, res) => {
-      const data = req.body
-      const id = req.params.id
-      const buscaFeed = FeedRepository.buscarFeed()
+      buscarTodosUsuarios.then((usuarios) => {
+        usuarios.forEach((usuario) => {
+          if (usuario.id == id) {
+            const usuarioBuscado = usuario;
+            const buscarFeed = FeedRepository.buscarFeedPorUsuario(
+              usuarioBuscado.id
+            );
 
-      buscaFeed.then((result) => {
-        res.status(204).json(FeedRepository.updateFeed(data, result[id - 1].id))
-      })
-    })
+            buscarFeed.then((feed) => {
+              res.status(200).json(feed);
+            });
+          }
+        });
+      });
+    });
 
-    //Fazer a deleção do feed
+    /**
+     * Método para fazer update adicionando post ao feed por id
+     */
+    app.patch('/feed/:feedId/:postId', async (req, res) => {
+      const { feedId, postId } = req.params;
+      const { post } = req.body;
+    
+      try {
+        const updatedPost = await FeedRepository.postarFeed(feedId, postId, post);
+        res.status(200).json({
+          message: `Post atualizado com sucesso`,
+          updatedPost,
+        });
+      } catch (error) {
+        res.status(406).json({
+          error: true,
+          message: error.message,
+        });
+      }
+    });
+    
+    /**
+     * Método para deletar feed
+     */
     app.delete('/feed/:id', (req, res) => {
-      const id = req.params.id
-      const buscaFeed = FeedRepository.buscarFeed()
+      const id = req.params.id;
+      const buscarFeeds = FeedRepository.buscarFeed();
 
-      buscaFeed.then((result) => {
-        res.status(204).json(FeedRepository.deleteFeed(result[id - 1].id))
-      })
-    })
+      buscarFeeds.then((feed) => {
+        if (feed[id - 1]) {
+          const feedParaExcluir = feed[id - 1];
+          FeedRepository.deleteFeed(feedParaExcluir.id);
+          res.status(204).json();
+        } else {
+          res.status(404).json({
+            error: true,
+            message: `Usuário com o id ${id} não encontrado`,
+          });
+        }
+      });
+    });
   }
 }
 
-export default FeedController
+export default FeedController;
